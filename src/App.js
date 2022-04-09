0af8eb0ec7e25b9ee858ed6235757350877386b3
aMa-Production <nodejsanishmandal@gmail.com>
2022-04-09 06:20:50 +0545

    homepage timeline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-     <h4>Portfolio</h4>
-    </div>
+    <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
   );
 }
 
